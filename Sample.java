a9f5906b79f40151e0306ca4baeb70a34bce817d
Godugu Bhuvana kruthi <162475082+sitarama77@users.noreply.github.com>
2024-12-10 13:48:44 +0530

    Update Sample.java

--- Sample.java
+++ Sample.java
@@ -4,7 +4,7 @@ public class Sample {
 
     public static void main(String[] args) {
 
-        Scanner reader = new Scanner(System.in);
+        
 
         System.out.print("Enter a number: ");
         int num = reader.nextInt();
