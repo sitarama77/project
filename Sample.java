98780c1b990bdeb88bbdba0815ab172d96c40c9f
Godugu Bhuvana kruthi <162475082+sitarama77@users.noreply.github.com>
2024-12-10 13:52:01 +0530

    Update Sample.java

--- Sample.java
+++ Sample.java
@@ -1,17 +1,13 @@
 
 
-public class Sample {
-
-    public static void main(String[] args) {
-
-        
-
-        System.out.print("Enter a number: ");
-        int num = nextInt();
-
-        if(num % 2 == 0)
-            System.out.println(num + " is even");
-        else
-            System.out.println(num + " is odd");
-    }
-}
+public class Sample
+ {
+      public static void main(String[] args) {
+           int n= 51;
+     //checking whether the number is even or odd
+     if (n% 2 == 0)
+              System.out.println(n + " is Even");
+     else
+              System.out.println(n + " is odd");
+      }
+ }
